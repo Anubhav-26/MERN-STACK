c762d684e1946eac3f27fb00a9dcf1ddc13a8b99
Anubhav Agrahari <anubhavagrahari53@gmail.com>
2026-04-07 16:16:45 +0530

    Initial commit

---
+++ JavaScript/3Decision Control/switch.js
@@ -0,0 +1,127 @@
+
+let day = 8;
+
+if (day === 1) {
+  console.log("Monday");
+} else if (day === 2) {
+  console.log("Tuesday");
+} else if (day === 3) {
+  console.log("Wednesday");
+} else if (day === 4) {
+  console.log("Thursday");
+} else if (day === 5) {
+  console.log("Friday");
+} else if (day === 6) {
+  console.log("Saturday");
+} else if (day === 7) {
+  console.log("Sunday");
+} else {
+  console.log("Invalid day");
+}
+// let day = 8;
+// if(day===1){
+//   console.log("monday");
+// }
+// else if(day===2){
+//   console.log("Tuesday");
+// }
+// else if (day===3){
+//   console.log("wednesday");
+// }
+// else if(day===4);{
+//    console.log("Thursday");
+// }else if(day===5){
+//   console.log("friday");
+
+// }
+// else if(day===6){
+//   console.log("saturday");
+// }
+// else if(day===7){
+//   console.log("Sunday");
+// }
+// else{
+//   console.log("Invalid-day");
+// }
+
+// let day = 8;
+
+// if (day === 1) {
+//   console.log("Monday");
+// } else if (day === 2) {
+//   console.log("Tuesday");
+// } else if (day === 3) {
+//   console.log("Wednesday");
+// } else if (day === 4) {
+//   console.log("Thursday");
+// } else if (day === 5) {
+//   console.log("Friday");
+// } else if (day === 6) {
+//   console.log("Saturday");
+// } else if (day === 7) {
+//   console.log("Sunday");
+// } else {
+//   console.log("Invalid day");
+// }
+
+
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+    break;
+}
+
+
+
+
+
+// switch(day){
+//   case 1:
+//     console.log("sunday");
+//     break;
+//     case 2:
+//       console.log("Monday");
+//       break;
+//       case 3:
+//         console.log("tuesday");
+//         break;
+//         case 4:
+//           console.log("wednesday");
+//           break;
+//           case 5:
+//             console.log("Thursaday");
+//             break;
+//             case 6:
+//               console.log("friday");
+//               break;
+//               case 7:
+//                 console.log("saturday");
+//                 break;
+//                 default :
+//                   console.log("invalid-day")
+
+
+
+// }
